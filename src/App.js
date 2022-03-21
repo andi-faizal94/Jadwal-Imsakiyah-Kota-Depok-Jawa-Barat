d0c99bb6c402bf8368ba9a9e25213cb7c8040c43
Andi Faizal <andifaizal647@gmail.com>
2022-03-21 20:55:30 +0700

    add jawa barat in heading

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
         <h1 className="text-white">Jadwal</h1>
         <h1 className="text-white">Imsakiyah</h1>
         <h2 className="text-white">Kota Depok</h2>
+        <h2 className="text-white">Jawa Barat</h2>
         <h2 className="text-white">Ramadhan 1443 H/2022 M</h2>
       </div>
       <Row className="mt-4 mb-2">
